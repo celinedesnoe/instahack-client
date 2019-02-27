b6d18754f2ed4028a94110409249699ff36c339f
celinedesnoe <celine.desnoe@skema.edu>
2019-02-27 22:41:44 +0100

    Header welcome to IG

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header" />
+        <header className="App-header">WELCOME INSTAGRAM </header>
       </div>
     );
   }
