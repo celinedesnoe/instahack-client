1ad8683bc30a0203edf712c4b4570a1c6f684773
Anoopdeep Bal <anoopdeep@gmail.com>
2019-02-27 18:05:17 +0100

    ab: packages installed and app.js updated and api.js created

---
+++ src/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+// create an Axios object with pre-configured settings
+const backendApi = axios.create({
+  baseURL: "http://localhost:5555"
+});
+
+// allows us to replace axios.get("http://localhost:5555/api/phones") with axios.get("/api/phones");
+
+function errorHandler(err) {
+  if (err.response && err.response.data) {
+    console.log("API Error", err.response.data);
+  } else {
+    console.log("React Code Error", err);
+  }
+
+  // alert a generic message for the user
+  alert("Sorry! Something went wrong. Try again later.");
+
+  // cause the error again so the .then() won't be called
+  throw err;
+}
