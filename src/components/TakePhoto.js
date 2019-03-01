40fa461b2456e83393e7fb78f834679b9f283377
Anoopdeep Bal <anoopdeep@gmail.com>
2019-03-01 13:04:27 +0100

    ab: created file for TakePhoto component

---
+++ src/components/TakePhoto.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class TakePhoto extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <section className="TakePhoto">
+        <h2>TAKE PHOTO COMPONENT</h2>
+      </section>
+    );
+  }
+}
+
+export default TakePhoto;
