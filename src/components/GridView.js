76c03dfc73287a4ce396e738e6806877636539e2
celinedesnoe <celine.desnoe@skema.edu>
2019-03-01 11:42:57 +0100

    GridView removed useless stuff

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -1,26 +1,9 @@
 import React, { Component } from "react";
 import "./GridView.css";
-import { getUserProfile } from "../api";
-
 import { Link } from "react-router-dom";
 
 class GridView extends Component {
-  // componentDidMount() {
-  //   const { profileUser } = this.props;
-  //   console.log("User profile in GridView", profileUser);
-
-  //   getUserProfile(profileUser.username)
-  //     .then(
-  //       response => console.log("Posts Data", response.data.postResults)
-  //       // this.setState({ posts: response.data })
-  //     )
-  //     .catch(() => {
-  //       alert("Sorry we can't find the posts from the user");
-  //     });
-  // }
-
   render() {
-    // const { images } = this.state;
     const { profilePosts } = this.props;
 
     return (
@@ -43,22 +26,6 @@ class GridView extends Component {
           })}
         </div>
       </div>
-
-      //  <div className="GridView container-fluid">
-      //     <div className="row">
-      //       {images.map(oneImage => {
-      //         return (
-      //           <div className="col-4 myCol p-0">
-      //             <div className="square">
-      //               <a href={"/p/" + }>
-      //                 <img src={oneImage} alt="oneImage" className="square-img" />
-      //               </a>
-      //             </div>
-      //           </div>
-      //         );
-      //       })}
-      //     </div>
-      //   </div>
     );
   }
 }
