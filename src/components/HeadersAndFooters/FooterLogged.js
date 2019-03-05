1bfc908a36d171e4a2ea8db46522327be6a8ec2e
Estelle Olawinski <estelle.olawinski@me.com>
2019-03-05 20:06:43 +0100

    ok

--- src/components/HeadersAndFooters/FooterLogged.js
+++ src/components/HeadersAndFooters/FooterLogged.js
@@ -1,20 +1,46 @@
 import React, { Component } from "react";
 
 import "./FooterLogged.css";
-import profilewhite from "../../images/profilewhite.png";
+import homeblack from "../../images/homeblack.png";
+import searchblack from "../../images/searchblack.png";
+import plusblack from "../../images/plusblack.png";
+import likeblack from "../../images/like2black.png";
 
 class FooterLogged extends Component {
   render() {
     return (
       <div className="FooterLogged">
-        FOOTER LOGGED
-        <form className=" upload-btn-wrapper">
-          <input class="edit-button" type="file" />
-          <button class="btn">
-            {" "}
-            <img src={profilewhite} alt="profile icon" />
-            CAMERA
-          </button>
+        <form className="upload-btn-wrapper">
+          <div className="home">
+            <button class="btn">
+              <img src={homeblack} alt="home icon" />
+            </button>
+          </div>
+
+          <div className="search">
+            <button class="btn">
+              <img src={searchblack} alt="search icon" />
+            </button>
+          </div>
+
+          <div className="plus">
+            <input class="edit-button" type="file" />
+            <button class="btn">
+              <img src={plusblack} alt="profile icon" />
+            </button>
+          </div>
+
+          <div className="likes">
+            <button class="btn">
+              <img src={likeblack} alt="like icon" />
+            </button>
+          </div>
+
+          <div className="profile">
+            <button class="btn">
+              <img src={searchblack} alt="profile icon" />
+            </button>
+          </div>
         </form>
       </div>
       // NEED TO LAUNCH THE CAMERA OR TO CHOOSE A PICTURE FROM THE IPHONE
