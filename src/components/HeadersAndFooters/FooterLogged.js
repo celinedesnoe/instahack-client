01b14ad58a82bfb89f3626f4356e4b930cd4bcb5
Estelle Olawinski <estelle.olawinski@me.com>
2019-03-06 17:05:33 +0100

    some colouredIcon & redirect pages

--- src/components/HeadersAndFooters/FooterLogged.js
+++ src/components/HeadersAndFooters/FooterLogged.js
@@ -4,7 +4,9 @@ import { Redirect, Route } from "react-router-dom";
 import homeempty from "../../images/homeempty.png";
 import homefull from "../../images/homefull.png";
 import searchempty from "../../images/searchempty.png";
+import searchfull from "../../images/searchfull.png";
 import likeempty from "../../images/likeempty.png";
+import likefull from "../../images/likefull.png";
 import profileempty from "../../images/profileempty.png";
 import profilefull from "../../images/profilefull.png";
 
@@ -18,33 +20,102 @@ class FooterLogged extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      homed: false
+      homed: false,
+      searched: false,
+      liked: false,
+      profiled: false
     };
   }
+  //   <img src={homefull} alt="home icon" className="okbutton" />
+  //   <img src={profilefull} alt="profile icon" className="okbutton" />
 
-  home(event) {
-    this.setState({ homed: true });
+  colouredHome() {
+    this.setState({
+      homed: true,
+      searched: false,
+      liked: false,
+      profiled: false
+    });
   }
 
-  onClick(event) {}
+  colouredSearch() {
+    this.setState({
+      searched: true,
+      homed: false,
+      liked: false,
+      profiled: false
+    });
+  }
+
+  colouredLike() {
+    this.setState({
+      liked: true,
+      homed: false,
+      searched: false,
+      profiled: false
+    });
+  }
+
+  colouredProfile() {
+    this.setState({
+      profiled: true,
+      homed: false,
+      searched: false,
+      liked: false
+    });
+  }
 
   render() {
     return (
       <div className="FooterLogged">
         <div className="home">
-          <button class="buton">
-            {<Newsfeed /> ? (
-              <img src={homefull} alt="home icon" className="okbutton" />
-            ) : (
-              <img src={homeempty} alt="home icon" className="okbutton" />
-            )}
-          </button>
+          {this.state.homed ? (
+            <button class="buton">
+              <img
+                onClick={event => this.colouredHome(event)}
+                // className="Unlike"
+                src={homefull}
+                alt="home empty"
+                className="okbutton"
+              />
+              <Redirect to="/" />
+            </button>
+          ) : (
+            <button class="buton">
+              <img
+                onClick={event => this.colouredHome(event)}
+                // className="Like"
+                src={homeempty}
+                alt="home full"
+                className="okbutton"
+              />
+            </button>
+          )}
         </div>
 
         <div className="search">
-          <button class="buton">
-            <img src={searchempty} alt="search icon" className="okbutton" />
-          </button>
+          {this.state.searched ? (
+            <button class="buton">
+              <img
+                onClick={event => this.colouredSearch(event)}
+                // className="Unlike"
+                src={searchfull}
+                alt="search empty"
+                className="okbutton"
+              />
+              <Redirect to="/accounts/signup" />
+            </button>
+          ) : (
+            <button class="buton">
+              <img
+                onClick={event => this.colouredSearch(event)}
+                // className="Like"
+                src={searchempty}
+                alt="search full"
+                className="okbutton"
+              />
+            </button>
+          )}
         </div>
 
         <div className="plus">
@@ -52,19 +123,49 @@ class FooterLogged extends Component {
         </div>
 
         <div className="likes">
-          <button class="buton">
-            <img src={likeempty} alt="like icon" className="okbutton" />
-          </button>
+          {this.state.liked ? (
+            <button class="buton">
+              <img
+                onClick={event => this.colouredLike(event)}
+                src={likefull}
+                alt="like empty"
+                className="okbutton"
+              />
+              <Redirect to="/accounts/login" />
+            </button>
+          ) : (
+            <button class="buton">
+              <img
+                onClick={event => this.colouredLike(event)}
+                src={likeempty}
+                alt="like full"
+                className="okbutton"
+              />
+            </button>
+          )}
         </div>
 
         <div className="profile">
-          <button class="buton">
-            {<Profile /> ? (
-              <img src={profilefull} alt="profile icon" className="okbutton" />
-            ) : (
-              <img src={profileempty} alt="profile icon" className="okbutton" />
-            )}
-          </button>
+          {this.state.profiled ? (
+            <button class="buton">
+              <img
+                onClick={event => this.colouredProfile(event)}
+                src={profilefull}
+                alt="profile empty"
+                className="okbutton"
+              />
+              <Redirect to="/:username" />
+            </button>
+          ) : (
+            <button class="buton">
+              <img
+                onClick={event => this.colouredProfile(event)}
+                src={profileempty}
+                alt="profile full"
+                className="okbutton"
+              />
+            </button>
+          )}
         </div>
       </div>
       // NEED TO LAUNCH THE CAMERA OR TO CHOOSE A PICTURE FROM THE IPHONE
