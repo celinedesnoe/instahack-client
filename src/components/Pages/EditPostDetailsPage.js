01b40d00d7ba5ddd0799a7d4cc358472c87a1f7b
celinedesnoe <celine.desnoe@skema.edu>
2019-03-06 15:36:23 +0100

    CSS for Post

--- src/components/Pages/EditPostDetailsPage.js
+++ src/components/Pages/EditPostDetailsPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ProfilePic from "../General/ProfilePic.js";
 import { Link } from "react-router-dom";
 import { newPost } from "../../api.js";
+import ArrowGoBack from "../../images/arrowbackbold.png";
 
 import "./EditPostDetailsPage.css";
 
@@ -34,25 +35,49 @@ class EditPostDetailsPage extends Component {
     console.log("STATE", this.state);
     console.log("PROPS", this.props.props);
     return (
-      <div className="EditPostDetailsPage">
-        Edit post detail page
-        <ProfilePic
-          profilePic={this.props.currentUser.profilePic}
-          size="profile-row"
-        />
-        <textarea
-          class="caption-input"
-          onChange={event => this.genericOnChange(event)}
-          type="text"
-          rows="1"
-          name="caption"
-          placeholder="Write a caption..."
-        />
-        <img src={this.props.props.location.state.image} alt="uploaded" />
-        {/* TO PUT IN THE HEADER */}
-        <Link onClick={event => this.save(event)} to={`/`}>
-          Share
-        </Link>
+      <div className="EditPostDetailsPage w-100">
+        {/* HEADER */}
+        <div className={`headerAge ${this.props.position}`}>
+          <a href={this.props.link}>
+            <img className="arrow-back" src={ArrowGoBack} alt="Arrow Go Back" />
+          </a>
+
+          <p className="newPhotoPost">New Post</p>
+
+          <Link
+            onClick={event => this.save(event)}
+            to={`/`}
+            className="link-next"
+          >
+            Share
+          </Link>
+        </div>
+
+        <div className="margin-top container">
+          <div className="row row-margin">
+            <div className="col-9 d-flex">
+              <ProfilePic
+                profilePic={this.props.currentUser.profilePic}
+                size="profile-row"
+              />
+              <textarea
+                class="caption-input caption-margin-left"
+                onChange={event => this.genericOnChange(event)}
+                type="text"
+                rows="1"
+                name="caption"
+                placeholder="Write a caption..."
+              />
+            </div>
+
+            <img
+              src={this.props.props.location.state.image}
+              alt="uploaded"
+              className="col-3 w-100 contain-img"
+            />
+            {/* TO PUT IN THE HEADER */}
+          </div>
+        </div>
       </div>
     );
   }
