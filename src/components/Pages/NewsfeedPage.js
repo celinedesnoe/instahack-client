f53444b7cf948a64057e0a6e5d97b00dc24fd948
Anoopdeep Bal <anoopdeep@gmail.com>
2019-03-07 17:19:45 +0100

    ab: infinite scroll ready to test part 2

--- src/components/Pages/NewsfeedPage.js
+++ src/components/Pages/NewsfeedPage.js
@@ -12,36 +12,91 @@ class Newsfeed extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: [],
-      hasMoreItems: true
+      postsAvailable: [],
+      postsToRender: [],
+      hasMoreItems: true,
+      nextIndex: null
     };
   }
 
   componentDidMount() {
     // fnc exported from api.js
     // queries to get a giant array of posts from each user someone is following
-    getNewsfeedPosts(this.props.currentUser).then(response => {
-      console.log("ALL POSTS in NEWSFEED: ", response.data);
-      console.log(this.props);
-
-      // this.state.postsToRender.unshift(response.data);
-      this.setState({ postsToRender: response.data });
-    });
+    // getNewsfeedPosts(this.props.currentUser).then(response => {
+    //   console.log("ALL POSTS in NEWSFEED: ", response.data);
+    //   console.log(this.props);
+    //   this.setState({ postsToRender: response.data });
+    // });
   }
 
   loadItems(page) {
     var self = this;
 
-    getNewsfeedPosts(this.props.currentUser).then(response => {
-      console.log("ALL POSTS in NEWSFEED: ", response.data);
-      if (response) {
-        var posts = self.state.posts;
+    if (self.state.postsAvailable.length < 10) {
+      getNewsfeedPosts({
+        user: this.props.currentUser,
+        nextIndex: this.state.nextIndex
+      }).then(response => {
+        console.log("NEXT 109 POSTS in NEWSFEED: ", response.data);
+        if (response.data.length > 0) {
+          var posts = self.state.postsAvailable;
+          console.log("posts in IF: ", posts.length);
 
-        response.data.map(onePost => {
-          posts.push(onePost);
-        });
-      }
-    });
+          response.data.map(oneResponse => {
+            posts.push(oneResponse);
+          });
+
+          const newNext = self.state.postsToRender.length + posts.length;
+          console.log("NEW NEXT: ", newNext);
+
+          self.setState({
+            postsAvailable: posts,
+            nextIndex: newNext
+          });
+          // } else {
+          //   self.setState({
+          //     hasMoreItems: false
+          //   });
+        } else {
+          console.log(
+            "REMAINING POSTS: ",
+            self.state.postsAvailable.length > 0
+          );
+
+          if (self.state.postsAvailable.length > 0) {
+            const next10 = self.state.postsAvailable.splice(0, 10);
+
+            // console.log("available: ", self.state.postsAvailable);
+            // console.log("next 10: ", self.state.next10);
+
+            next10.forEach(onePost => {
+              self.state.postsToRender.push(onePost);
+            });
+
+            self.setState({
+              postsToRender: self.state.postsToRender
+            });
+          } else {
+            self.setState({
+              hasMoreItems: false
+            });
+          }
+        }
+      });
+    } else {
+      const next10 = self.state.postsAvailable.splice(0, 10);
+
+      // console.log("available: ", self.state.postsAvailable);
+      // console.log("next 10: ", self.state.next10);
+
+      next10.forEach(onePost => {
+        self.state.postsToRender.push(onePost);
+      });
+
+      // console.log("to render: ", self.state.postsToRender);
+      // console.log("avail length: ", self.state.postsAvailable.length);
+      self.setState({ postsToRender: self.state.postsToRender });
+    }
   }
 
   render() {
@@ -53,7 +108,7 @@ class Newsfeed extends Component {
     );
 
     let items = [];
-    this.state.posts.map(onePost => {
+    this.state.postsToRender.map(onePost => {
       items.push(
         <PostDetailPage
           className="newsfeed-post"
@@ -78,6 +133,7 @@ class Newsfeed extends Component {
           loadMore={this.loadItems.bind(this)}
           hasMore={this.state.hasMoreItems}
           loader={loader}
+          initialLoad={true}
         >
           <div className="newsfeed">{items}</div>
         </InfiniteScroll>
