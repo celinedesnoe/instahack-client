80b12172df4010266fe376f46358b2ec63f2562f
celinedesnoe <celine.desnoe@skema.edu>
2019-03-19 18:04:28 +0100

    fixed

--- src/components/EditAccount/AddUsernameAndPsw.js
+++ src/components/EditAccount/AddUsernameAndPsw.js
@@ -6,10 +6,7 @@ import "./AddUsernameAndPsw.css";
 class AddUsernameAndPsw extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      fullName: "",
-      originalPassword: ""
-    };
+    this.state = {};
   }
   render() {
     return (
